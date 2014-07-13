842daab07a61ab6ab9279f0370baac1fb01a43a1
Alonso Mendez <alonsomendez@gmail.com>
2014-07-13 06:47:46 +0000

    A socket container, to encapsulate the connector behavior, also is sending a null key to the server to provoke an screen update.

---
+++ app/js/sockets.js
@@ -0,0 +1,24 @@
+;(function(io, window) {
+  'use strict';
+
+  var Sockets = function(){
+    var self = {
+        connector: null
+      };
+
+    self.connect = function(playerName) {
+      self.connector.emit('connection', playerName);
+      self.connector.emit(CONFIG.events.onPlayerMove, CONFIG.keys.none);
+    };
+
+    self.init = function() {
+      self.connector =  io();
+    };
+
+    return self;
+  }();
+
+  Sockets.init();
+
+  window.Sockets = Sockets;
+})(io, window);
